aa9be411a131d8f05e254ed04aa3c006cc5f1808
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-18 07:31:02 -0800

    File renamed to Student.cs, constructor addded along with parameters Name and Age

---
+++ Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace COMP003A.LectureAcivity6
+
+public class Student
+{  //Fields
+    public string Name;
+    public int Age;
+
+    public Student(string name, int age) //constructor
+    {
+        Name = name;
+        Age = age;
+    }
+}
+//constructor
