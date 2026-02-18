4e6e4a34bf4926d86714ccacf7a611f62355f5bc
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-18 08:39:09 -0800

    added comments for the reflection questions

--- Program.cs
+++ Program.cs
@@ -1,13 +1,33 @@
 using System;
 
-namespace COMP003A.LectureActivity6;
-
-class Program
+namespace COMP003A.LectureActivity6
 {
-    static void Main(string[] args)
+    class Program
     {
-        //2 student objects
-        Student student1 = new Student("Alex", 20);
-        Student student2 = new Student("Jordan", 22);
+        static void Main(string[] args)
+        {
+            //2 student objects
+            Student student1 = new Student("Alex", 20);
+            Student student2 = new Student("Jordan", 22);
+
+            //call instance methods
+            student1.DisplayInfo();
+            student2.DisplayInfo();
+
+            //modifying one object
+            student1.Age = 32;
+
+            //display again
+            student1.DisplayInfo();
+            student2.DisplayInfo();
+        }
     }
+
 }
+/* Reflection:
+1. Each Student object has its own data because we use the constructor with the new keyword this results
+in a seperate instance of student class being created in mmebory with its own Name and Age stored on its own
+2. Main doesnt need to know because it calls the method using dot notation.
+3. We use encapsulation by having the Student class group its own data and the command that operates on that 
+data. The internal implementation is contained in the class and the rest of the program just interacts with it.
+*/
